7a0a54c2d533ea79298423aa2cb07fcfcd261356
Vanessa Martinez <vmar76@gmail.com>
2020-02-13 03:10:25 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,7 +32,7 @@
 
 def naming_system
   hash = {
-    PORTION_1: => {
+    :PORTION_1 => {
       label: "Kingdom",
       sub_category: {
       label: "Phylum",
@@ -41,16 +41,16 @@ def naming_system
     }
   }
 },
-    PORTION_2: => {
+    :PORTION_2 => {
       label: "Order"
 },
-    PORTION_3: => {
+    :PORTION_3 => {
       label: "Family",
       sub_category: {
       label: "Genus",
   }
 },
-    PORTION_4: => {
+    :PORTION_4 => {
       label: "Species",
       sub_category: nil 
   }
