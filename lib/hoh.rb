721d088a5353b8e8e87939d24492d83192938f2d
Vanessa Martinez <vmar76@gmail.com>
2020-02-13 03:45:49 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,29 +31,28 @@
 # }
 
 def naming_system
-  hash = {
     {
       label: "Kingdom",
       sub_category: {
-      label: "Phylum",
-      sub_category: {
-      label: "Class"
+        label: "Phylum",
+        sub_category: {
+          label: "Class",
+          sub_category: {
+            label: "Order"
+            sub_category: {
+              label: "Family",
+              sub_category: {
+                label: "Genus",
+                sub_category: {
+                  label: "Species",
+                  sub_category: nil
+                }
+              }
+            }
+          }
+        }
+      }
     }
-  }
-},
-    PORTION_2 = {
-      label: "Order"
-},
-    PORTION_3 = {
-      label: "Family",
-      sub_category: {
-      label: "Genus",
-  }
-},
-    PORTION_4 = {
-      label: "Species",
-      sub_category: nil 
-  }
   hash[:sub_category][:sub_category][:sub_category][:label]="Order"
   hash
 end
