7a6682321b0ac180992ce1fc8da4cc0bea5c3393
Vanessa Martinez <vmar76@gmail.com>
2020-02-13 03:09:53 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,16 +41,16 @@ def naming_system
     }
   }
 },
-    PORTION_2 => {
+    PORTION_2: => {
       label: "Order"
 },
-    PORTION_3 => {
+    PORTION_3: => {
       label: "Family",
       sub_category: {
       label: "Genus",
   }
 },
-    PORTION_4 => {
+    PORTION_4: => {
       label: "Species",
       sub_category: nil 
   }
